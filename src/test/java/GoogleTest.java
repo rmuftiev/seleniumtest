6a6ce192e47031d014f29c74ed88854e16757bd5
“rmuftievgit config --global user.email “rasim.muftiev@live.com <rasim.muftiev@gmail.com>
2021-11-25 16:43:55 +0100

    selenium test commit 1

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class GoogleTest {
+
+    @Test
+
+    public void testGoogle () {
+        String baseUrl = "https://www.google.com/";
+        String chromeDriverPath="/Users/rasim.muftiev/selenium/chromedriver";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+
+        String URL= driver.getCurrentUrl();
+
+        String title = driver.getTitle();
+
+        String completeSourceCode = driver.getPageSource();
+
+        driver.close(); //Closed opened window
+        //   driver.quit(); closes all opened window
+    }
+
+
+}
